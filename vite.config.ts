79b91e929a00cdb56863d5021b716a843d6e1128
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2025-10-03 22:40:18 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -24,4 +25,9 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '../createLucideIcon.js': resolve(__dirname, 'node_modules/lucide-vue-next/dist/esm/createLucideIcon.js'),
+        },
+    },
 });
